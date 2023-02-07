b42ba05519a52027d5cd845358bbc429e49bbf6a
Denys Almaral <pxtracer@gmail.com>
2023-02-07 15:42:13 -0400

    gittest 5, after merging dev to main

--- PaintCursor.cs
+++ PaintCursor.cs
@@ -13,3 +13,4 @@ namespace DAPolyPaint
 //more test git stuff 2
 //gittest: after pull request, an accidentally merged main to dev :P
 //gittest4: on more commit to dev
+//gittest5: after merging dev to main, another commit to dev
